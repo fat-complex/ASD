a5c9a4de0ee876fcc6731af5b895522d2ea20bf8
andrey.tutyne <andrey.tutyne@avroid.tech>
2024-09-12 15:22:33 +0300

    dyn array - multi dimension tests

--- part_1/dynamic_array/multi_dimension_array.py
+++ part_1/dynamic_array/multi_dimension_array.py
@@ -1,4 +1,6 @@
 from part_1.dynamic_array.dyn_array import DynArray
+import unittest
+
 
 class MultiDimensionArray(DynArray):
 
@@ -15,7 +17,27 @@ class MultiDimensionArray(DynArray):
                 arr.append(arg)
         return arr
 
+class TestMultiDimension(unittest.TestCase):
+    def test_add_elem(self):
+        arr = MultiDimensionArray.make(*[[[1], [2], [3]],  # row 0
+                                         [[4], [5], [6]]]) # row 1
+        arr[1][1].append(23)
+        # Result:
+        # [[[1], [2], [3]], row 0
+        #  [[4], [5, 23], [6]]] row 1
+
+        self.assertEqual(arr[1][1][0], 5)
+        self.assertEqual(arr[1][1][1],  23)
+
+        # add row
+        arr.insert(len(arr), [[44], [45], [46]])
+        self.assertEqual(arr[2][0][0], 44)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 arr = MultiDimensionArray.make(*[[[1], [2], [3]],
                                  [[4], [5], [6]]])
 
-arr[1][1].append(23)
