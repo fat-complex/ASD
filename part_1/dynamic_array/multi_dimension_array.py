ec3c5dddba18e68565ac76d1fe0a10308e7a418a
andrey.tutyne <andrey.tutyne@avroid.tech>
2024-09-12 13:30:10 +0300

    dyn array - multi dimension

---
+++ part_1/dynamic_array/multi_dimension_array.py
@@ -0,0 +1,16 @@
+from part_1.dynamic_array.dyn_array import DynArray
+
+class MultiDimensionArray(DynArray):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def make(*args):
+        arr: MultiDimensionArray = MultiDimensionArray()
+        for arg in args:
+            if isinstance(arg, list):
+                arr.append(MultiDimensionArray.make(*arg))
+            else:
+                arr.append(arg)
+        return arr
