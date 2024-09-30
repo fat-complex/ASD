0b6ddc6cf6168cc6cc64fe907c52fe9fb39a40d4
andrey.tutyne <andrey.tutyne@avroid.tech>
2024-09-30 12:11:28 +0300

    Bloom filter - basic tests

---
+++ part_1/bloom_filter/bloom_filter.py
@@ -0,0 +1,33 @@
+class BloomFilter:
+
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.bloom_filter: int = 0
+
+
+    def hash1(self, str1):
+        h = 0
+        for c in str1:
+            code = ord(c)
+            h = (h * 17 + code) % self.filter_len
+        return 1 << h
+
+    def hash2(self, str1):
+        h = 0
+        for c in str1:
+            code = ord(c)
+            h = (h * 223 + code) % self.filter_len
+        return 1 << h
+
+    def add(self, str1):
+        self.__set(self.hash1(str1))
+        self.__set(self.hash2(str1))
+
+    def is_value(self, str1):
+        return self.__is_set(self.hash1(str1)) and self.__is_set(self.hash2(str1))
+
+    def __set(self, bit_mask):
+        self.bloom_filter |= bit_mask
+
+    def __is_set(self, bit_mask) -> bool:
+        return bit_mask & self.bloom_filter != 0
---
+++ part_1/bloom_filter/tests_bloom_filter.py
@@ -0,0 +1,45 @@
+import unittest
+
+from part_1.bloom_filter.bloom_filter import BloomFilter
+
+
+class TestAddMethod(unittest.TestCase):
+    def test_add(self):
+        bloom = BloomFilter(32)
+
+        str1 = "0123456789"
+        str2 = "1234567890"
+        str3 = "2345678901"
+        str4 = "3456789012"
+        str5 = "4567890123"
+        str6 = "5678901234"
+        str7 = "6789012345"
+        str8 = "7890123456"
+        str9 = "8901234567"
+        str10 = "9012345678"
+
+        bloom.add(str1)
+
+        self.assertEqual(bloom.is_value(str1), True)
+        self.assertEqual(bloom.is_value(str2), False)
+        self.assertEqual(bloom.is_value(str3), True)
+        self.assertEqual(bloom.is_value(str4), False)
+        self.assertEqual(bloom.is_value(str5), True)
+        self.assertEqual(bloom.is_value(str6), False)
+        self.assertEqual(bloom.is_value(str7), True)
+        self.assertEqual(bloom.is_value(str8), False)
+
+        bloom.add(str2)
+        self.assertEqual(bloom.is_value(str2), True)
+
+        bloom.add(str9)
+        self.assertEqual(bloom.is_value(str9), True)
+
+        bloom.add(str10)
+        self.assertEqual(bloom.is_value(str10), True)
+
+        self.assertEqual(bloom.is_value("sdfsdfsdfsdfs"), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
